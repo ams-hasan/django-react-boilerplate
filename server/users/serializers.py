ff7359f248955c84876c49a507ed447e562169c9
ams-hasan <amsh.himu@gmail.com>
2020-10-12 19:16:18 +0600

    fix(api): used kwargs instead of static arguments in create_user

--- server/users/serializers.py
+++ server/users/serializers.py
@@ -7,20 +7,17 @@ from .models import User
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
-        fields = ('id', 'email', 'first_name', 'last_name')
+        fields = ("id", "email", "first_name", "last_name")
 
 
 class RegisterSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
-        fields = ('id', 'email', 'first_name', 'last_name', 'password')
-        extra_kwargs = {'password': {'write_only': True}}
+        fields = ("id", "email", "first_name", "last_name", "password")
+        extra_kwargs = {"password": {"write_only": True}}
 
     def create(self, validated_data):
-        user = User.objects.create_user(validated_data['email'],
-                                        validated_data['first_name'],
-                                        validated_data['last_name'],
-                                        validated_data['password'])
+        user = User.objects.create_user(**validated_data)
         return user
 
 
@@ -32,4 +29,6 @@ class LoginSerializer(serializers.Serializer):
         user = authenticate(**data)
         if user and user.is_active:
             return user
-        raise serializers.ValidationError("Unable to log in with the provided credentials")
+        raise serializers.ValidationError(
+            "Unable to log in with the provided credentials"
+        )
--- server/users/urls.py
+++ server/users/urls.py
@@ -4,12 +4,12 @@ from rest_framework import routers, views
 from .views import UserViewSet, RegisterAPI, LoginAPI, LogoutAPI
 
 router = routers.DefaultRouter()
-router.register('users', UserViewSet)
+router.register("users", UserViewSet, basename="users")
 
 urlpatterns = [
-    path('register/', RegisterAPI.as_view(), name='register'),
-    path('login/', LoginAPI.as_view(), name='login'),
-    path('logout/', LogoutAPI.as_view(), name='logout'),
+    path("register/", RegisterAPI.as_view(), name="register"),
+    path("login/", LoginAPI.as_view(), name="login"),
+    path("logout/", LogoutAPI.as_view(), name="logout"),
 ]
 
 urlpatterns += router.urls
